6eaf55bcae95abbe5868bc1222aaf25a0bb04b94
Sal Tal <110280707+Saltal27@users.noreply.github.com>
2022-09-18 22:15:04 +0000

    nothing

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 # 🚨 Don't change the code below 👇
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+    student_scores[n] = int(student_scores[n])
 print(student_scores)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
 heighest_score = 0
 for each_score in student_scores:
-    if each_score > heighest_score :
+    if each_score > heighest_score:
         heighest_score = each_score
 print(f"The highest score in the class is: {heighest_score}")
-
-
-
